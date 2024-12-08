f189cec645eb28c312343355fe1dce5840fdbf73
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-08 11:52:19 +0530

    Modified File

--- QuestionSet1.py
+++ QuestionSet1.py
@@ -51,3 +51,58 @@
 #     print(f"{string} is not palindrome string.")
 
 
+# Q6. Take two strings as input and concatenate them without using the + operator.
+# str1 = "Hello"
+# str2 = "World"
+# print(str1+str2)   # HelloWorld
+
+
+# Q7. Take a list of strings as input and join them into a single string separated by spaces.
+# worlds = input("Enter a list of words separated by spaces : ").split()
+# print(type(worlds), worlds)
+# result = " ".join(worlds)
+# print("Joined String : ", result)
+
+
+# Q8. Take an input as a string, convert it into an integer and a float, and display all three values.
+# data = input("Enter String ")
+# str_val = data
+# int_val = int(data)
+# float_val = float(data)
+# print(f"String : {str_val}, Integer : {int_val}, Float : {float_val}")
+
+
+
+# Q9. Take a string input containing digits and letters and extract only the digits.
+# Example:
+# Input: abc123xyz
+# Output: 123
+
+# step 1
+# digits = []
+# data = input("Enter value combination of string and digit : ")
+# print(data)
+# for i in data:
+#     if i.isdigit():
+#         digits.append(i)
+# print("".join(digits))
+
+# step 2 
+# data = input("Enter value combination of string and digit : ")
+# digits = ''.join(filter(str.isdigit, data))
+# print("Digits : ", digits)
+
+
+# Q10. Take a number as input and print its reverse.
+# num = input("Enter The Number")
+# reversed_num = num[::-1]
+# print(reversed_num)
+
+
+# Q11. Check Leap Year
+year = int(input("Enter the year : "))
+if (year % 4 == 0 and year % 100 !=  0) or (year % 400 == 0):
+    print(f"{year} is leap year")
+else :
+    print(f"{year} is not leap year")
+
