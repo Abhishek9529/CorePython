89e466125a4909cab1c886a56a76370cf5015210
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-08 08:49:40 +0530

    Modified File

--- QuestionSet1.py
+++ QuestionSet1.py
@@ -1,8 +1,16 @@
-# Q1 Write a program to swap the values of two variables without using a third variable.
-a = 10 
-b = 20
-print (f"Before Swaping  Values of a = {a} And b = {b}")
-c = a
-a = b
-b = c
-print (f"After Swaping  Values of a = {a} And b = {b}")
+# Q1. Write a program to swap the values of two variables without using a third variable.
+# a = 10 
+# b = 20
+# print (f"Before Swaping  Values of a = {a} And b = {b}")
+# c = a
+# a = b
+# b = c
+# print (f"After Swaping  Values of a = {a} And b = {b}")
+
+
+# Q2. Write a program that takes input from the user and checks its data type.
+try :
+    a = int(input("Enter the Value : "))
+    print(f" The type of a is : {type(a)}")
+except ValueError as e:
+    print("Entered Value is Not a Int")
