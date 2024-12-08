59a34109cbbc93b614151c57e1c90cf118d69c2d
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-08 08:23:43 +0530

    Add QuestionSet1.py File

---
+++ QuestionSet1.py
@@ -0,0 +1,8 @@
+# Q1 Write a program to swap the values of two variables without using a third variable.
+a = 10 
+b = 20
+print (f"Before Swaping  Values of a = {a} And b = {b}")
+c = a
+a = b
+b = c
+print (f"After Swaping  Values of a = {a} And b = {b}")
