2d7360202d3cd9e1768cfaadc4677117541ac994
Abhishek9529 <abhishekchavhan61047@gmail.com>
2024-12-09 08:56:27 +0530

    Modified File

--- QuestionSet1.py
+++ QuestionSet1.py
@@ -100,9 +100,9 @@
 
 
 # Q11. Check Leap Year
-year = int(input("Enter the year : "))
-if (year % 4 == 0 and year % 100 !=  0) or (year % 400 == 0):
-    print(f"{year} is leap year")
-else :
-    print(f"{year} is not leap year")
+# year = int(input("Enter the year : "))
+# if (year % 4 == 0 and year % 100 !=  0) or (year % 400 == 0):
+#     print(f"{year} is leap year")
+# else :
+#     print(f"{year} is not leap year")
 
